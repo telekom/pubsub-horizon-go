391f71ab7ac4f5fecd82ae26ff36a8182f6d1fb7
Jens Fischer <jens-fischer@telekom.de>
2024-05-29 10:46:42 +0200

    fix: use string type for time field in event

--- pkg/message/event.go
+++ pkg/message/event.go
@@ -1,16 +1,12 @@
 package message
 
-import (
-	"time"
-)
-
 type Event struct {
-	Id              string    `json:"id" validate:"required,uuid4"`
-	Type            string    `json:"type" validate:"required,eventType"`
-	Source          string    `json:"source" validate:"required"`
-	SpecVersion     string    `json:"specVersion" validate:"required"`
-	DataContentType string    `json:"dataContentType"`
-	DataRef         string    `json:"dataRef"`
-	Time            time.Time `json:"time" validate:"isoTime"`
-	Data            any       `json:"data"`
+	Id              string `json:"id" validate:"required,uuid4"`
+	Type            string `json:"type" validate:"required,eventType"`
+	Source          string `json:"source" validate:"required"`
+	SpecVersion     string `json:"specVersion" validate:"required"`
+	DataContentType string `json:"dataContentType"`
+	DataRef         string `json:"dataRef"`
+	Time            string `json:"time" validate:"isoTime"`
+	Data            any    `json:"data"`
 }
