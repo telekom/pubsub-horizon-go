a8ec0351891d5824f8bcd21b8a47d2a923456db8
Jens Fischer <jens-fischer@telekom.de>
2024-07-16 15:22:23 +0200

    feat: implement Timestamp type

---
+++ types/timestamp_test.go
@@ -0,0 +1,32 @@
+package types
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestCircuitBreakerTime_MarshalJSON(t *testing.T) {
+	var assertions = assert.New(t)
+	var dummyTime = time.Date(1, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	bytes, err := dummyTime.MarshalJSON()
+	assertions.NoError(err)
+	assertions.Equal(`"0001-01-01T00:00:00Z"`, string(bytes))
+}
+
+func TestCircuitBreakerTime_UnmarshalJSON(t *testing.T) {
+	var assertions = assert.New(t)
+	var expectation = time.Date(1, 1, 1, 0, 0, 0, 0, time.UTC)
+	var bytes = []byte(`{"time": "0001-01-01T00:00:00Z"}`)
+
+	var dummy = struct {
+		Timestamp Timestamp `json:"time"`
+	}{}
+
+	assertions.NoError(json.Unmarshal(bytes, &dummy))
+
+	var unmarshalledTime = dummy.Timestamp.ToTime()
+	assertions.True(unmarshalledTime.Equal(expectation))
+}
