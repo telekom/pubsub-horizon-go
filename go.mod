e6a6b8373b295f00fee4fe88bf5ae277e66fe526
Jens Fischer <jens-fischer@telekom.de>
2024-07-25 15:29:24 +0200

    chore(license): update license information and switch from dep5 to REUSE.toml

--- go.mod
+++ go.mod
@@ -3,10 +3,15 @@ module github.com/telekom/pubsub-horizon-go
 go 1.22
 
 require (
+	github.com/go-playground/validator/v10 v10.20.0
 	github.com/hazelcast/hazelcast-go-client v1.4.1
 	github.com/ory/dockertest/v3 v3.10.0
 	github.com/rs/zerolog v1.33.0
 	github.com/stretchr/testify v1.9.0
+	go.opentelemetry.io/contrib/propagators/b3 v1.28.0
+	go.opentelemetry.io/otel v1.28.0
+	go.opentelemetry.io/otel/sdk v1.28.0
+	go.opentelemetry.io/otel/trace v1.28.0
 )
 
 require (
@@ -23,14 +28,15 @@ require (
 	github.com/docker/go-connections v0.5.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.2.4 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
@@ -49,11 +55,13 @@ require (
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
-	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
-	golang.org/x/tools v0.21.0 // indirect
+	go.opentelemetry.io/otel/metric v1.28.0 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/tools v0.23.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- tracing/tracing_test.go
+++ tracing/tracing_test.go
@@ -1,1 +1,91 @@
+// Copyright 2024 Deutsche Telekom IT GmbH
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package tracing
+
+import (
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/contrib/propagators/b3"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/sdk/resource"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/sdk/trace/tracetest"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+	"os"
+	"testing"
+	"time"
+)
+
+var (
+	traceExporter *tracetest.InMemoryExporter
+)
+
+func TestMain(m *testing.M) {
+	traceExporter = configureTestProvider()
+	os.Exit(m.Run())
+}
+
+func configureTestProvider() *tracetest.InMemoryExporter {
+	var exporter = tracetest.NewInMemoryExporter()
+	var provider = tracesdk.NewTracerProvider(
+		tracesdk.WithSyncer(exporter),
+		tracesdk.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("horizon-go"),
+		)),
+	)
+	otel.SetTracerProvider(provider)
+
+	var b3Propagator = b3.New(b3.WithInjectEncoding(b3.B3MultipleHeader))
+	otel.SetTextMapPropagator(b3Propagator)
+
+	return exporter
+}
+
+func TestTraceContext_StartSpan(t *testing.T) {
+	var assertions = assert.New(t)
+	var traceCtx = NewTraceContext("myservice", false)
+	defer traceExporter.Reset()
+
+	traceCtx.StartSpan("myspan")
+	traceCtx.EndCurrentSpan()
+
+	var snapshots = traceExporter.GetSpans().Snapshots()
+	assertions.Equal(1, len(snapshots))
+
+	var firstSpan = snapshots[0]
+	assertions.Equal("myspan", firstSpan.Name())
+	assertions.Equal(0, firstSpan.ChildSpanCount())
+	assertions.LessOrEqual(firstSpan.EndTime(), time.Now())
+
+	assertions.Nil(traceCtx.CurrentSpan())
+	assertions.NotNil(traceCtx.LastSpan())
+}
+
+func TestTraceContext_StartDetailedSpan(t *testing.T) {
+	t.Run("detailed enabled", func(t *testing.T) {
+		var assertions = assert.New(t)
+		var detailedTraceCtx = NewTraceContext("myservice", true)
+		defer traceExporter.Reset()
+
+		detailedTraceCtx.StartDetailedSpan("mydetailedspan")
+		detailedTraceCtx.EndCurrentDetailedSpan()
+
+		var snapshots = traceExporter.GetSpans().Snapshots()
+		assertions.Equal(1, len(snapshots))
+		assertions.Equal("mydetailedspan", snapshots[0].Name())
+	})
+
+	t.Run("detailed disabled", func(t *testing.T) {
+		var assertions = assert.New(t)
+		var regularTraceCtx = NewTraceContext("myservice", false)
+		defer traceExporter.Reset()
+
+		regularTraceCtx.StartDetailedSpan("mydetailedspan")
+		regularTraceCtx.EndCurrentDetailedSpan()
+
+		var snapshots = traceExporter.GetSpans().Snapshots()
+		assertions.Equal(0, len(snapshots))
+	})
+}
