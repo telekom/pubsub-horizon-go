c5a2780ff783b85c4f4ed409f44ab4c10bba76b8
Jens Fischer <jens-fischer@telekom.de>
2025-04-17 16:00:36 +0200

    fix(event): change capitalization of SpecVersion field in Event

--- message/event.go
+++ message/event.go
@@ -8,7 +8,7 @@ type Event struct {
 	Id              string `json:"id" validate:"required,uuid4"`
 	Type            string `json:"type" validate:"required,eventType"`
 	Source          string `json:"source" validate:"required"`
-	SpecVersion     string `json:"specVersion" validate:"required"`
+	SpecVersion     string `json:"specversion" validate:"required"`
 	DataContentType string `json:"dataContentType"`
 	DataRef         string `json:"dataRef,omitempty"`
 	Time            string `json:"time" validate:"omitempty,isoTime"`
